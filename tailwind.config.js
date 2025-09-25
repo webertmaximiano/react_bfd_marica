a70b7096c4da9e6d968b27464a4584ab6542a5f6
Webert Maximiano <webertcoach@gmail.com>
2025-09-25 14:18:26 -0300

    aula 15 iniciada

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./fluxo_sala_de_aula_parque_tecnologico/aula-15/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
